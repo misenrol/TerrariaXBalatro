364932e33432c3952483ff5d4c433efc2cf4e362
Rosette <165220598+Row-set-te@users.noreply.github.com>
2025-03-30 01:59:34 -0400

    Update brainofcthulhu.lua
    will no longer create a tarot per card scored

--- modules/jokers/brainofcthulhu.lua
+++ modules/jokers/brainofcthulhu.lua
@@ -17,26 +17,30 @@ SMODS.Joker{
     end,
 
     calculate = function(self, card, context)
-            if context.cardarea == G.play and context.individual and next(context.poker_hands['Flush']) and context.other_card:is_suit('Hearts') then
-                if (#G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit) then
-                    G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
-    
-                    G.E_MANAGER:add_event(Event({
-                        func = function()
-                            SMODS.add_card({ set = 'Tarot' })
-                            
-                            G.GAME.consumeable_buffer = 0   
+        if  context.cardarea == G.play and
+            context.individual and
+			context.other_card:is_suit('Hearts') and
+            next(context.poker_hands['Flush'])
+		then
+            if (#G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit) then
+                G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
 
-                            card_eval_status_text(
-                            context.blueprint_card or card,
-                            'extra', nil, nil, nil,
-                            { message = "Confused", colour = G.C.RED }
-                            )
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        SMODS.add_card({ set = 'Tarot' })
+                        
+                        G.GAME.consumeable_buffer = 0   
 
-                            return true
-                        end
-                    }))
-                end
+                        card_eval_status_text(
+                        context.blueprint_card or card,
+                        'extra', nil, nil, nil,
+                        { message = "Confused", colour = G.C.RED }
+                        )
+
+                        return true
+                    end
+                }))
             end
+        end
     end
 }
