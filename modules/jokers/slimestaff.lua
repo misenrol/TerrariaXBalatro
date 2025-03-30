35c89f00997b658d06e816235b9dbb958ff71049
Rosette <165220598+Row-set-te@users.noreply.github.com>
2025-03-30 01:17:40 -0400

    Update slimestaff.lua
    adding base x1 mult so staff doesnt give x0.75 when there is only 1 joker in play

--- modules/jokers/slimestaff.lua
+++ modules/jokers/slimestaff.lua
@@ -18,7 +18,7 @@ SMODS.Joker{
     calculate = function(self, card, context)
         if context.cardarea == G.jokers and context.joker_main then
             return{ 
-                xchips = (G.jokers and G.jokers.cards and #G.jokers.cards or 0)*card.ability.extra.xchips
+                xchips = 1 + (G.jokers and G.jokers.cards and #G.jokers.cards or 0)*card.ability.extra.xchips
             }
         end
     end
